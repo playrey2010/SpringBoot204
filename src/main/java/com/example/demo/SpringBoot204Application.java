1a9e0ae1eac5781af22c47cea318eabccb718c48
playrey2010 <playrey2010@gmail.com>
2019-07-14 19:24:25 -0400

    Form validation

---
+++ src/main/java/com/example/demo/SpringBoot204Application.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringBoot204Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot204Application.class, args);
+    }
+
+}
